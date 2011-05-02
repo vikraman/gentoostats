5b654c655741caf0c032ad7626f69849b59a2c25
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2011-05-02 20:01:46 +0530

    Use portage.settings instead of forking to emerge --info

--- client/environment.py
+++ client/environment.py
@@ -1,19 +1,9 @@
 
 import logging
-import subprocess
+import portage
 
 class Environment(object):
 
-  def __init__(self):
-    try:
-      p = subprocess.Popen(['emerge', '--info'], stdout=subprocess.PIPE)
-      self.out = p.stdout.readlines()
-    except OSError, e:
-      fatal('Cannot run emerge --info')
-      raise e
-
   def getVar(self, myvar):
-    for line in self.out:
-      if line.startswith(myvar):
-	return line.strip()
-    return ''
+    ret = portage.settings[myvar]
+    return ret
