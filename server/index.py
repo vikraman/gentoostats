d7869829c9591d6f3e8dddc05a97cdcdfc1975f2
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2011-06-16 02:25:03 +0530

    fix index page

--- server/index.py
+++ server/index.py
@@ -3,6 +3,6 @@ from config import render, db
 
 class Index(object):
   def GET(self):
-  	hosts = db.select('hosts', what='count(uuid) as count')
-	count = hosts[0].count
+  	hosts = db.select('HOSTS', what='COUNT(UUID) as COUNT')
+	count = hosts[0]['COUNT']
 	return render.index(count)
