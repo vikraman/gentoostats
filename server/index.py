6a6a09d88f50282b910ef82c47bb4bb21cd48556
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2011-08-04 05:15:24 +0530

    updates to index page, trying out relative urls

--- server/index.py
+++ server/index.py
@@ -1,8 +1,22 @@
 
+from web import form
 from config import render, db
+
+search_form = form.Form(
+        form.Textbox('cat', value = 'any', description = 'Category'),
+        form.Textbox('pkg', value = 'any', description = 'Package'),
+        form.Textbox('ver', value = 'any', description = 'Version'),
+        form.Textbox('repo', value = 'any', description = 'Repository'),
+        form.Textbox('min_hosts', value = 'any', description = 'Minimum hosts'),
+        form.Textbox('max_hosts', value = 'any', description = 'Maximum hosts'),
+        form.Button('Search', type = 'submit')
+        )
 
 class Index(object):
     def GET(self):
         hosts = db.select('HOSTS', what='COUNT(UUID) as COUNT')
         count = hosts[0]['COUNT']
-        return render.index(count)
+
+        form = search_form()
+
+        return render.index(count, form)
